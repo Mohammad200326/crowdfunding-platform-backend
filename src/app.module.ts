20aa219917e2c667db06d36b9ebba3638f8095ad
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2026-01-05 15:08:21 +0200

    feat: setup config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import path from 'path';
+import { ConfigModule } from '@nestjs/config';
+
+const envFilePath = path.join(
+  __dirname,
+  `../.env.${process.env.NODE_ENV === 'development' ? 'dev' : 'prod'}`,
+);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
