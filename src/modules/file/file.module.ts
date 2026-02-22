c20ae199a03a3bc4ab3c2e470ab9affc6a138933
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2026-02-22 17:16:04 +0200

    fix file filter

--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -15,10 +15,13 @@ import { ImageKitProvider } from './imagekit.provider';
           limits: {
             fileSize: 2 * 1024 * 1024,
           },
+          // fileFilter: (req, file, cb) => {
+          //   if (!file.mimetype.startsWith('image/')) {
+          //     return cb(new Error('Only image files are allowed'), false);
+          //   }
+          //   cb(null, true);
+          // },
           fileFilter: (req, file, cb) => {
-            if (!file.mimetype.startsWith('image/')) {
-              return cb(new Error('Only image files are allowed'), false);
-            }
             cb(null, true);
           },
         };
